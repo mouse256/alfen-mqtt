2e16adeee90635113adc55565b035413dd81f7a6
Tom Billiet <mouse256@gmail.com>
2026-06-18 08:07:25 +0200

    Fix compilation issue

--- src/main/java/org/muizenhol/alfen/AlfenModbusClient.java
+++ src/main/java/org/muizenhol/alfen/AlfenModbusClient.java
@@ -334,7 +334,8 @@ public class AlfenModbusClient implements AutoCloseable {
                             uuid,
                             "mouse256",
                             "alfen",
-                            "alfen-mqtt"
+                            "alfen-mqtt",
+                            null
                     )
                     , new Discovery.Origin(
                     "alfen-mqtt"
